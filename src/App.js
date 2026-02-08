8f506be79ee3472c2606248cdeb77cfd945aa455
mirzadev <mudabbirmirza2712@gmail.com>
2026-02-08 23:22:47 +0530

    fix dependency resolution and restore working app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import Intro from "./components/Intro";
-import Experience from "./components/Experience";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Credits from "./components/Credits";
-import NavBar from "./components/NavBar";
-import "./App.css";
-import "./styles/Global.css";
-import "rsuite/dist/styles/rsuite-default.css";
+import React from "react"
+import Intro from "./components/Intro"
+import Experience from "./components/Experience"
+import About from "./components/About"
+import Projects from "./components/Projects"
+import Credits from "./components/Credits"
+import NavBar from "./components/NavBar"
+import "./App.css"
+import "./styles/Global.css"
+import "rsuite/dist/styles/rsuite-default.css"
 
 function App() {
   return (
@@ -21,7 +21,7 @@ function App() {
         <Credits></Credits>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
